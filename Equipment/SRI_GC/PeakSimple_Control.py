a2e4d1cc86984f0a04074e7ba4168870be7ec9ac
Briley Bourgeois <bbourge6@stanford.edu>
2021-11-10 20:50:24 -0800

    updated SRI code

--- Equipment/SRI_GC/PeakSimple_Control.py
+++ Equipment/SRI_GC/PeakSimple_Control.py
@@ -8,9 +8,11 @@ Created on Thu Oct 14 17:30:17 2021
 import clr  # Weirdly this is python.NET
 import sys
 
-assemblydir = ('C:/Program Files/Added Programs/PeakSimple_Control/'
-               'PeakSimple_Control_Demo/PeaksimpleClient/PeaksimpleConnector')
+# assemblydir = ('C:/Users/brile/source/repos/Dionne_Lab/photoreactor/Equipment/'
+#                'SRI_GC/PeaksimpleClient')
 
+assemblydir = (r'C:\\Program Files\\Added Programs\\PeakSimple_Control\\'
+               'PeakSimple_Control_Demo\\PeaksimpleClient\\PeaksimpleConnector.dll')
 sys.path.append(assemblydir)  # Make sure the assembly is in path
 clr.AddReference(assemblydir) # Add the assembly to python.NET
 import Peaksimple  # Import the assembly namespace, which has a different name
@@ -18,9 +20,9 @@ import Peaksimple  # Import the assembly namespace, which has a different name
 Connector = Peaksimple.PeaksimpleConnector()  # This class has all the functions
 
 Channel = int(1)
-ctrl_file = 'C:\\Program Files\\Added Programs\\PeakSimple version 4.89Win10\\DEFAULT2.CON'
-ctrl_file = 'Vial1.CON'
-#"C:\Program Files\Added Programs\PeakSimple version 4.89Win10\333calibration.ASC"
+ctrl_file = 'C:/Program Files/Added Programs/PeakSimple version 4.89Win10/DEFAULT2.CON'
+# ctrl_file = 'Vial1.CON'
+#"C:/Program Files/Added Programs/PeakSimple version 4.89Win10/333calibration.ASC"
 Connector.Connect()
 Connector.LoadControlFile(ctrl_file)
 
