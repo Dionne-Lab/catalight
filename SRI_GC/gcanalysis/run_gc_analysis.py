38eb670c5468003e13c63fc689e27b3517d8beb1
Briley Bourgeois <bbourge6@stanford.edu>
2022-01-07 10:40:07 -0800

    completed analysis code with functionality for power and temp sweeps. additional commenting needed.

--- SRI_GC/gcanalysis/run_gc_analysis.py
+++ SRI_GC/gcanalysis/run_gc_analysis.py
@@ -96,39 +96,90 @@ def run_analysis(Expt1, calDF, basecorrect='True'):
     std.to_csv(os.path.join(expt_results_fol, 'std_conc.csv'))
     return(results, avg, std)
 
-def plot_results(Expt1, CalDF):
+def plot_results(Expt1, calDF, s, reactant, mass_bal='c', figsize=(6.5, 4.5)):
     # Plotting
     ###########################################################################
     print('Plotting...')
     plt.close('all')
-    plt.rcParams.update({'font.size': 14})
-    plt.rcParams['axes.linewidth'] = 1.5
-    calchemIDs = calDF.index.to_numpy() # get chem IDs from calibration files
     
-    fig1, ax1 = plt.subplots() # initilize run num plot
+    if figsize[0] < 6 : fontsize = [11, 14]
+    elif figsize[0] > 7: fontsize = [16, 20]
+    else: fontsize = [14, 18]
+                
+    # Some Plot Defaults
+    plt.rcParams['axes.linewidth'] = 2
+    plt.rcParams['lines.linewidth'] = 1.5
+    plt.rcParams['xtick.major.size'] =  6
+    plt.rcParams['xtick.major.width'] = 1.5
+    plt.rcParams['ytick.major.size'] =  6
+    plt.rcParams['ytick.major.width'] = 1.5
+    plt.rcParams['figure.figsize'] = figsize
+    plt.rcParams['font.size'] = fontsize[0]
+    plt.rcParams['axes.labelsize'] = fontsize[1]
+    
+    # Initilize run num plot
+    fig1, ax1 = plt.subplots() 
+    # Calculations:
+    calchemIDs = calDF.index.to_numpy() # get chem IDs from calibration files
+    stoyk = []
+
+    # Plotting:
     for chem_num in range(len(calchemIDs)):
         chemical = calchemIDs[chem_num]
+        
+        # read number after 'c' in each chem name
+        stoyk.append(re.findall( mass_bal+r"(\d+)", chemical))
         ind_results = results[:, chem_num, :]
         ind_results = ind_results[~np.isnan(ind_results)]
         ax1.plot(ind_results, 'o', label=chemical)
-        ax1.legend()
-        
-    plt.xlabel('Run Number (#)', fontsize=18)
-    plt.ylabel('Conc (ppm)', fontsize=18)
-    plt.gca().tick_params(which='both', width=1.5, length=6)
-    plt.gca().tick_params(which='minor', width=1.5, length=3)
+    
+    ax1.legend()    
+    plt.xlabel('Run Number (#)')
+    plt.ylabel('Conc (ppm)')
     plt.tight_layout()
     
-    ax2 = avg.plot(marker='o', yerr=std)
-    fig2 = ax2.get_figure()
-    ax2.set_xlabel('Temperature (K)', fontsize=18)
-    ax2.set_ylabel('Conc (ppm)', fontsize=18)
+    # Initilize ppm vs ind_var plot
+    fig2, ax2 = plt.subplots() 
+    # Calculations:
+    units = (Expt1.expt_list['Units']
+             [Expt1.expt_list['Active Status']].to_string(index=False))
+    x_data = getattr(Expt1, Expt1.ind_var)
+    mol_count = avg @ (np.array(stoyk, dtype=int))
+    mol_count.columns = ['Total ' + mass_bal]
+    mol_error = std @ (np.array(stoyk, dtype=int))
+
+    # Plotting:
+    avg[0:len(x_data)].plot(ax=ax2, marker='o', yerr=std)
+    mol_count[0:len(x_data)].plot(ax=ax2, marker='o', yerr=mol_error[0:len(x_data)])
+    ax2.set_xlabel(Expt1.ind_var + ' ['+units+']')
+    ax2.set_ylabel('Conc (ppm)')
     plt.tight_layout()
     
-    fig1.savefig(os.path.join(Expt1.results_path, 'run_num_plot.svg'), format="svg")
-    fig2.savefig(os.path.join(Expt1.results_path, 'avg_conc_plot.svg'), format="svg")
+    # Initilize Conv and Selectivity plot
+    fig3, ax3 = plt.subplots()
+    # Calculations:
+    C_Tot = avg.sum(axis=1) # total conc of all molecules
+    C_reactant = avg[reactant] # total conc of reactant molecule
+    X = (1 - C_reactant/C_Tot)*100 # conversion assuming 100% mol bal
+    rel_err = std/avg
+    X_err = ((rel_err**2).sum(axis=1))**(1/2)*rel_err[reactant]
+    S = (avg[s[0]]/(C_Tot*X/100))*100 # selectivity
     
-    return (ax1, ax2)
+    # Plotting:
+    X[0:len(x_data)].plot(ax=ax3, yerr=X_err*X, fmt='--o')
+    S[0:len(x_data)].plot(ax=ax3, yerr=X_err*S, fmt='--^')
+    ax3.set_xlabel(Expt1.ind_var + ' ['+units+']')
+    ax3.set_ylabel('Conv./Selec. [%]')
+    plt.legend(['Conversion', 'Selectivity'])
+    ax3.set_ylim([0, 100])
+    plt.tight_layout()
+                              
+    # Figure Export
+    fig1.savefig(os.path.join(Expt1.results_path, str(figsize[0])+'w_run_num_plot.svg'), format="svg")
+    fig2.savefig(os.path.join(Expt1.results_path, str(figsize[0])+'w_avg_conc_plot.svg'), format="svg")
+    fig3.savefig(os.path.join(Expt1.results_path, str(figsize[0])+'w_Conv_Sel_plot.svg'), format="svg")
+    
+    return (ax1, ax2, ax3)
 if __name__ == "__main__":
     
     # User inputs
@@ -154,16 +205,23 @@ if __name__ == "__main__":
     # Sample Location Info:
     log_path = (r'C:/Users/brile/Documents/Temp Files/'
                 '20210524_8%AgPdMix_1wt%__200C_24.8mg/PostReduction/expt_log.txt')
-    
+    main_dir = "G:\\Shared drives\\Hydrogenation Projects\\AgPd Polyhedra\\Ensemble Reactor\\202111_pretreatment_tests\\"
     # Main Script
     ###########################################################################
-    # import all calibration data
-    calDF = pd.read_csv(calibration_path, delimiter=',', index_col='Chem ID') 
-    Expt1 = Experiment()
-    Expt1.read_expt_log(log_path)
-    Expt1.update_save_paths(log_path)
-    
-    (results, avg, std) = run_analysis(Expt1, calDF)
-    (ax1, ax2) = plot_results(Expt1, calDF)
-
+    for dirpath, dirnames, filenames in os.walk(main_dir):
+       for filename in filenames: 
+           if filename == 'expt_log.txt':
+                log_path = os.path.join(dirpath, filename)
+                # import all calibration data
+                calDF = pd.read_csv(calibration_path, delimiter=',', index_col='Chem ID') 
+                Expt1 = Experiment() # Initialize experiment obj
+                Expt1.read_expt_log(log_path) # Read expt parameters from log
+                Expt1.update_save_paths(log_path) # update file paths
+                
+                (results, avg, std) = run_analysis(Expt1, calDF)
+                (ax1, ax2, ax3) = plot_results(Expt1, calDF, s=['c2h4'], 
+                                               reactant='c2h2', figsize=(4.35, 3.25))
+# Standard figsize 
+# 1/2 slide = (6.5, 4.5);  1/6 slide = (4.35, 3.25); 
+# 1/4 slide =  (5, 3.65); Full slide =    (9, 6.65);
     print('Finished!')
