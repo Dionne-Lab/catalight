2d26d18823634b70ec1f1bfed007dee1e4175028
D-Lab <dionnelab@gmail.com>
2022-10-28 17:12:28 -0700

    editted to include newly purchased mfc and additional com ports

--- equipment/alicat_MFC/connection_tester.py
+++ equipment/alicat_MFC/connection_tester.py
@@ -7,17 +7,19 @@ Created on Wed Jan 19 15:32:48 2022
 from alicat import FlowController, FlowMeter
 
 
-for port_name in ['COM6', 'COM7', 'COM8', 'COM9']:
-    for address_name in ['A', 'B', 'C', 'D']:
+for port_name in ['COM6', 'COM7', 'COM8', 'COM9', 'COM10', 'COM11']:
+    for address_name in ['A', 'B', 'C', 'D', 'E']:
         if FlowController.is_connected(port_name, address=address_name):
             print(port_name + ' ' + address_name + ' is flow controller')
+            break
         elif FlowMeter.is_connected(port_name, address=address_name):
             print(port_name + ' ' + address_name + ' is flow meter')
+            break
         
-for port_name in ['COM1', 'COM3', 'COM5']:
-    for address_name in ['A', 'B', 'C', 'D']:
-        if FlowController.is_connected(port_name, address=address_name):
-            print(port_name + ' ' + address_name + ' is flow controller')
-        elif FlowMeter.is_connected(port_name, address=address_name):
-            print(port_name + ' ' + address_name + ' is flow meter')
+# for port_name in ['COM1', 'COM2', 'COM3', 'COM4', 'COM5', 'COM11']:
+#     for address_name in ['A', 'B', 'C', 'D', 'E']:
+#         if FlowController.is_connected(port_name, address=address_name):
+#             print(port_name + ' ' + address_name + ' is flow controller')
+#         elif FlowMeter.is_connected(port_name, address=address_name):
+#             print(port_name + ' ' + address_name + ' is flow meter')
       
