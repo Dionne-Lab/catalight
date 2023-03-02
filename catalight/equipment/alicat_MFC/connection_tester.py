479fca001f053b08d13a682da38720c2975228d3
D-Lab <dionnelab@gmail.com>
2023-03-02 14:44:11 -0800

    made connection tester module run full test over all existing comports. No user interaction needed now.

--- catalight/equipment/alicat_MFC/connection_tester.py
+++ catalight/equipment/alicat_MFC/connection_tester.py
@@ -1,59 +1,37 @@
 """
-Created on Wed Jan 19 15:32:48 2022
+Port connection tester module returns the comport name/address of MFCs.
 
-@author: DLAB
+Utilizes the is_connected method of the alicat module's FlowController and
+FlowMeter classes. Usual serial module to list all comports on computer, then
+sweeps through every alphabetic letter in alphabet to checking for MFC
+connections. Each comport loop breaks if a MFC is found. Prints summary at end.
+
+Created on Wed Jan 19 15:32:48 2022
+@author: Briley Bourgeois
 """
+from string import ascii_uppercase as alphabet
+
+import serial.tools.list_ports
 from alicat import FlowController, FlowMeter
 
-for port_name in ['COM6', 'COM7', 'COM8', 'COM9', 'COM10', 'COM11']:
-    for address_name in ['A', 'B', 'C', 'D', 'E']:
+comports = list(serial.tools.list_ports.comports())
+positive_cases = []
+
+print('Beginning search: This process will take several minutes')
+for port in comports:
+    port_name = port.name
+    for address_name in alphabet:
+        test_case_str = (port_name + ' ' + address_name)
         if FlowController.is_connected(port_name, address=address_name):
-            print(port_name + ' ' + address_name + ' is flow controller')
+            print(test_case_str + ' is flow controller')
+            positive_cases.append(test_case_str + ' is flow controller\n')
             break
         elif FlowMeter.is_connected(port_name, address=address_name):
-            print(port_name + ' ' + address_name + ' is flow meter')
+            print(test_case_str + ' is flow meter')
+            positive_cases.append(test_case_str + ' is flow meter\n')
             break
-
-# for port_name in ['COM1', 'COM2', 'COM3', 'COM4', 'COM5', 'COM11']:
-#     for address_name in ['A', 'B', 'C', 'D', 'E']:
-#         if FlowController.is_connected(port_name, address=address_name):
-#             print(port_name + ' ' + address_name + ' is flow controller')
-#         elif FlowMeter.is_connected(port_name, address=address_name):
-#             print(port_name + ' ' + address_name + ' is flow meter')
-
-# import sys
-# import glob
-# import serial
-
-
-# def serial_ports():
-#     """ Lists serial port names
-
-#         :raises EnvironmentError:
-#             On unsupported or unknown platforms
-#         :returns:
-#             A list of the serial ports available on the system
-#     """
-#     if sys.platform.startswith('win'):
-#         ports = ['COM%s' % (i + 1) for i in range(256)]
-#     elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
-#         # this excludes your current terminal "/dev/tty"
-#         ports = glob.glob('/dev/tty[A-Za-z]*')
-#     elif sys.platform.startswith('darwin'):
-#         ports = glob.glob('/dev/tty.*')
-#     else:
-#         raise EnvironmentError('Unsupported platform')
-
-#     result = []
-#     for port in ports:
-#         try:
-#             s = serial.Serial(port)
-#             s.close()
-#             result.append(port)
-#         except (OSError, serial.SerialException):
-#             pass
-#     return result
-
-# import serial.tools.list_ports
-# print(list(serial.tools.list_ports.comports()))
-# []
+        else: 
+            print(test_case_str + ' is not MFC')
+            
+print('Search concluded. Results:')
+print(*positive_cases)
