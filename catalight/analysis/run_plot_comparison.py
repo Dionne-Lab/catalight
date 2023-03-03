8436c9070b9fd4c10af9f9915b2ccc6dae9f23db
Briley Bourgeois <bbourge6@stanford.edu>
2023-03-02 21:50:52 -0800

    added test comment to see how locking main branch works

--- catalight/analysis/run_plot_comparison.py
+++ catalight/analysis/run_plot_comparison.py
@@ -120,3 +120,4 @@ if __name__ == "__main__":
     plt.close('all')
     file_list, data_labels, response_dict = get_user_inputs()
     main(file_list, data_labels, **response_dict)
+    # Test comment
