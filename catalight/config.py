744b474bf28312417cfbccf737fd09891ee4ea1a
Briley <bbourge6@stanford.edu>
2023-06-05 10:57:16 -0700

    changed address for heater to int instead of str

--- catalight/config.py
+++ catalight/config.py
@@ -3,5 +3,5 @@ mfc_list = [{'port': 'COM7', 'unit': 'A'},
             {'port': 'COM4', 'unit': 'C'},
             {'port': 'COM3', 'unit': 'D'},
             {'port': 'COM5', 'unit': 'E'}]
-heater_address = {'port': 'COM9', 'address': '1'}
+heater_address = {'port': 'COM9', 'address': 1}
 peaksimple_path = r"C:\Users\dionn\GC\Peak495Win10\Peak495Win10.exe"
