12f4e6f43cd4a8242ef2c9c70662e5a42fcd59ee
Briley Bourgeois <bbourge6@stanford.edu>
2023-06-08 09:31:55 -0700

    comments

--- catalight/config.py
+++ catalight/config.py
@@ -1,8 +1,13 @@
+# This is the folder in which experimental data will be saved
+data_path = r"C:\Users\dionn\GC\GC_Data"
+
+# This is the full path to Peaksimple .exe
+peaksimple_path = r"C:\Users\dionn\GC\Peak495Win10\Peak495Win10.exe"
+
+# COM address info for hardware
 mfc_list = [{'port': 'COM7', 'unit': 'A'},
             {'port': 'COM6', 'unit': 'B'},
             {'port': 'COM4', 'unit': 'C'},
             {'port': 'COM3', 'unit': 'D'},
             {'port': 'COM5', 'unit': 'E'}]
 heater_address = {'port': 'COM9', 'address': '1'}
-peaksimple_path = r"C:\Users\dionn\GC\Peak495Win10\Peak495Win10.exe"
-data_path = r"C:\Users\dionn\GC\GC_Data"
