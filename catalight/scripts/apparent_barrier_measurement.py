7fbd18962f3f53aab70b60a4e6577183958cec8c
Briley <bbourge6@stanford.edu>
2024-06-10 16:06:11 -0700

    ran calibration of nkt and updated the activation barrier measurement script for final version

--- catalight/scripts/apparent_barrier_measurement.py
+++ catalight/scripts/apparent_barrier_measurement.py
@@ -19,7 +19,7 @@ from catalight.equipment.experiment_control import Experiment
 
 def initialize_equipment():
     ctrl_file = (r"C:\Users\dionn\GC\Control_Files\HayN_C2H2_Hydrogenation"
-                 r"\20221106_C2H2_Hydro_HayN_TCD_off.CON")
+                 r"\20240323_C2H2_Hydro_HayN_TCD_off.CON")
     gc_connector = GC_Connector(ctrl_file)
     laser_controller = NKT_System()
     gas_controller = Gas_System()
@@ -65,15 +65,29 @@ def run_study(expt_list, eqpt_list):
 if __name__ == "__main__":
     eqpt_list = initialize_equipment()  # Initialize equipment
     # Create folder to save data into
-    sample_name = '20230504_Au95Pd5_4wt_4mg'
-    main_fol = os.path.join(r"C:\Users\dionn\GC\GC_Data\20231225", sample_name)
+    sample_name = '20230504_Au95Pd5_4wt_3mg'
+    main_fol = os.path.join(r"C:\Users\dionn\GC\GC_Data\20240604", sample_name)
     os.makedirs(main_fol, exist_ok=True)
 
+    # Note, using the 10% mix of C2H2 in Ar
+    purge = Experiment(eqpt_list)
+    purge.expt_type = 'stability_test'
+    purge.gas_type = ['Ar', 'C2H2', 'H2', 'Ar']
+    purge.temp = [300]
+    purge.gas_comp = [[0.5, 0, 0, 0.5]]
+    purge.tot_flow = [100]
+    purge.sample_rate = 30
+    test_time = 60  # min
+    purge.sample_set_size = int(test_time // purge.sample_rate)
+    purge.t_steady_state = 30 + int(test_time % purge.sample_rate)
+    purge.sample_name = sample_name
+    purge.create_dirs(main_fol)
+    
     # Run a pre-experiment reduction
     reduction = Experiment(eqpt_list)
     reduction.expt_type = 'stability_test'
     reduction.gas_type = ['Ar', 'C2H2', 'H2', 'Ar']
-    reduction.temp = [300+273]
+    reduction.temp = [200+273]
     reduction.gas_comp = [[0, 0, 0.05, 0.95]]
     reduction.tot_flow = [50]
     reduction.sample_rate = 30
@@ -81,27 +95,45 @@ if __name__ == "__main__":
     reduction.t_steady_state = 30
     reduction.sample_name = sample_name
     reduction.create_dirs(main_fol)
-    try:
-        reduction.run_experiment()
-    except Exception as e:
-        shut_down(eqpt_list)
-        raise (e)
+    
+    # Condition the catalyst
+    stability_test = Experiment(eqpt_list)
+    stability_test.expt_type = 'stability_test'
+    stability_test.gas_type = ['Ar', 'C2H2', 'H2', 'Ar']
+    stability_test.temp = [380]
+    stability_test.gas_comp = [[0, 0.1, 0.05, 1-0.15]]
+    stability_test.tot_flow = [50]
+    stability_test.sample_rate = 30
+    test_time = 2000  # min
+    stability_test.sample_set_size = int(test_time // stability_test.sample_rate)
+    stability_test.t_steady_state = 30 + int(test_time % stability_test.sample_rate)
+    stability_test.sample_name = sample_name
+    stability_test.create_dirs(main_fol)
+    
+    # Make list of pre-experimental conditioning to run and loop through expts
+    pre_expts = [purge, reduction, stability_test]
+    for expt in pre_expts:
+        try:
+            expt.run_experiment()
+        except Exception as e:
+            shut_down(eqpt_list)
+            raise (e)
 
     # Main experiment
-    for wavelength in range(480, 680+1, 50):
-        for power in range(0, 60+1, 20):
+    for wavelength in range(480, 730+1, 50):
+        for power in range(0, 60+1, 15):
             expt = Experiment(eqpt_list)
             expt.expt_type = 'temp_sweep'
-            expt.temp = list(np.arange(340, 380+1, 10))
+            expt.temp = list(np.arange(310, 330+1, 5))
             expt.wavelength = [wavelength]
             expt.power = [power]
             expt.bandwidth = [50]
-            expt.gas_type = ['Ar', 'C2H2', 'H2', 'N2']
-            expt.gas_comp = [[1-0.06, 0.01, 0.05, 0]]
-            expt.tot_flow = [50]
+            expt.gas_type = ['Ar', 'C2H2', 'H2', 'Ar']
+            expt.gas_comp = [[0, 0.1, 0.05, 1-0.15]]
+            expt.tot_flow = [20]
             expt.sample_rate = 30
-            expt.sample_set_size = 4
-            expt.t_steady_state = 30
+            expt.sample_set_size = 3
+            expt.t_steady_state = 45
             expt.sample_name = sample_name
             expt.create_dirs(main_fol)
             try:
